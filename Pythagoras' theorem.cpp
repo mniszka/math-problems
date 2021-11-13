ca57cd76df08c69611f138f0d8e472119a674b0e
mniszka <aflisnik@wp.pl>
2021-11-13 10:26:54 +0100

    Update Pythagoras' theorem.cpp

--- Pythagoras' theorem.cpp
+++ Pythagoras' theorem.cpp
@@ -1,15 +1,21 @@
-// Check if three sides of a triangle can create a right triangle. We use the Pythagoras' theorem which is a relation among the three sides of a right triangle.
+/* We have given three square areas. Check if we can create a riht triangle if we put them together. One side of a square could be one side of a triangle. To solve the problem 
+we use the Pythagoras' theorem which is a relation among the three sides of a right triangle.*/
 
 #include <iostream>
+#include <math.h>
 #include <conio.h>
 
 using namespace std;
 
 int main(){
 
-int a=3,b=4,c=5; //pythagorean triple 
+int area_1=9,area_2=16,area_3=25;
+float side_a = sqrt(area_1);
+float side_b = sqrt(area_2);
+float side_c = sqrt(area_3);
 
-if((a*a+b*b) == (c*c) | (a*a+c*c)==(b*b) | (b*b+c*c) == (a*a)){
+if((side_a*side_a+side_b*side_b) == (side_c*side_c) | (side_a*side_a+side_c*side_c)==(side_b*side_b) |
+   (side_b*side_b+side_c*side_c) == (side_a*side_a)){
     cout<<"yes";
 }
 else
