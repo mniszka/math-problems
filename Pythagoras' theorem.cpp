2083dd0ee2a930af333080e305fce0124f609a60
mniszka <aflisnik@wp.pl>
2021-11-13 11:02:52 +0100

    Update Pythagoras' theorem.cpp

--- Pythagoras' theorem.cpp
+++ Pythagoras' theorem.cpp
@@ -9,13 +9,14 @@ using namespace std;
 
 int main(){
 
-int area_1=9,area_2=16,area_3=25; //square roots of these numbers is pythagorean triple  
+int area_1 = 9, area_2 = 16, area_3 = 25; //square roots of these numbers is pythagorean triple  
 double side_a = sqrt(area_1);
 double side_b = sqrt(area_2);
 double side_c = sqrt(area_3);
 
-if((side_a*side_a+side_b*side_b) == (side_c*side_c) | (side_a*side_a+side_c*side_c)==(side_b*side_b) |
-   (side_b*side_b+side_c*side_c) == (side_a*side_a)){
+if((side_a * side_a + side_b * side_b) == (side_c * side_c) | 
+   (side_a * side_a + side_c * side_c) == (side_b * side_b) |
+   (side_b * side_b + side_c * side_c) == (side_a * side_a)) {
     cout<<"yes";
 }
 else
