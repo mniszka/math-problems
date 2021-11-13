a8477cecac5d696a67d0191410d705662e3c86f3
mniszka <aflisnik@wp.pl>
2021-11-13 14:40:35 +0100

    Create Zero of a quadratic function.cpp

---
+++ Zero of a quadratic function.cpp
@@ -0,0 +1,33 @@
+//Find zeros of the function (what are values of x): y =3x^2 + 5 x+ 6
+
+#include <iostream>
+#include <conio.h>
+#include <math.h>
+
+using namespace std;
+
+int main(){
+
+float a=1, b=5, c=6, delta;
+delta = b*b - 4*a *c;
+
+if(delta<0){
+    cout<<"no zero";
+}
+
+else{
+if(delta==0){
+    cout<< -b/2*a;
+}
+
+else
+{
+    cout<<(-b - sqrt(delta)) /2*a<< " and " << ((-b + sqrt(delta)) /2*a);
+}
+}
+
+_getch();
+return 0;
+}
+
+
