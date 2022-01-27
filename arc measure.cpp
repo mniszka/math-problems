dc406488a102d83d2fd9beb5d5d5618c6777b39e
mniszka <aflisnik@wp.pl>
2022-01-27 13:10:52 +0100

    Create arc measure.cpp

---
+++ arc measure.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+#include<math.h>
+#define pi 3.141592653589793
+using namespace std;
+
+int main(){
+  int degree;
+  double radian;
+cout<<"Put a degree (angle)"<<endl;
+cin>>degree;
+radian = ((degree * pi)/180);
+cout<<radian;
+
+}
