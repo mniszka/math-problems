e18d9d79845cc8daf51d5c334e39c17b694efc7e
mniszka <aflisnik@wp.pl>
2022-03-03 20:38:25 +0100

    Update absolute value.cpp

--- absolute value.cpp
+++ absolute value.cpp
@@ -1,1 +1,41 @@
+//first soltion (without pointers):
 
+#include <iostream>
+using namespace std;
+
+int absoluteValue(int number){
+  if(number<0) return number*-1;
+  else return number;
+}
+
+int main() {
+
+  int number;
+  cout<<"Put a positive or negative number "<<endl;
+  cin>> number;
+  cout<<absoluteValue(number);
+  
+  return 0;
+}
+
+
+//second solution (with pointers):
+
+#include <iostream>
+using namespace std;
+
+void absoluteValue(int *number){
+  if(*number<0) *number = (*number*-1);
+  
+}
+
+int main() {
+
+  int number;
+  cout<<"Put a positive or negative number "<<endl;
+  cin>> number;
+  absoluteValue(&number);
+  cout<<number;
+  
+  return 0;
+}
