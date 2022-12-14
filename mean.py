d10d157e8831082229f98378a03cb68703ecf55a
Agnes Flisnik <aflisnik@wp.pl>
2022-12-14 14:44:38 +0100

    Update mean.py

--- mean.py
+++ mean.py
@@ -4,12 +4,23 @@ sum=0
 numbers_length = len(numbers)
 for i in numbers:
   sum+=i
+  
 numbers_mean = sum/numbers_length
 print(numbers_mean)
 
 
 #second way- using Python method
 import statistics
+
 numbers = [1,5,3]
 numbers_mean = statistics.mean(numbers)
 print(numbers_mean)
+
+#third way- using numpy library
+
+import numpy as np
+
+numbers = [1,5,3]
+numbers_mean = np.mean(numbers)
+print(numbers_mean)
+
