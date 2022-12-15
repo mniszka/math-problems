a0cfa721d0e948289eb26587492e4dfa393d01cc
Agnes Flisnik <aflisnik@wp.pl>
2022-12-15 16:06:58 +0100

    Update mean.py

--- mean.py
+++ mean.py
@@ -1,26 +1,47 @@
-#first way
+# MEAN (average) of numbers: 1,5,3:
+
+#first way:
 numbers = [1,5,3]
 sum=0
 numbers_length = len(numbers)
 for i in numbers:
   sum+=i
   
+#we can also use sum() function
+
 numbers_mean = sum/numbers_length
 print(numbers_mean)
 
 
-#second way- using Python method
+#second way- using Python method:
 import statistics
 
 numbers = [1,5,3]
 numbers_mean = statistics.mean(numbers)
 print(numbers_mean)
 
-#third way- using numpy library
+#third way- using numpy library:
 
 import numpy as np
 
 numbers = [1,5,3]
 numbers_mean = np.mean(numbers)
 print(numbers_mean)
+
+#HARMONIC MEAN:
+import statistics as s
+
+a= [1,5,3]
+
+print(s.harmonic_mean(a))
+
+
+#GEOMETRIC MEAN:
+import statistics as s
+
+a= [1,5,3]
+
+print(s.geometric_mean(a))
+
+
 
